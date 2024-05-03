174c235439994e8b52b0f56af223c2c475019409
Rene Ruano <rene.ruano95@gmail.com>
2024-05-03 00:50:56 -0400

    added sign-in page & sign-up page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignInForm from "@/components/sign-in/sign-in-form";
+
+export default function SignInPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <SignInForm />
+    </div>
+  );
+}
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "@/components/sign-up/sign-up-form";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <SignUpForm />
+    </div>
+  );
+}
