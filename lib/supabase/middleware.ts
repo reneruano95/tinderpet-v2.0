48298e628da5ddc89f4316a9fb93885f4cf2cb5a
Rene Ruano <rene.ruano95@gmail.com>
2024-05-05 18:13:21 -0400

    added middleware

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
-export const createClient = async (request: NextRequest) => {
+export const updateSession = async (request: NextRequest) => {
   // Create an unmodified response
   let response = NextResponse.next({
     request: {
