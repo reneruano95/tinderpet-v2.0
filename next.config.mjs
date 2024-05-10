b55c94cb57f5d1afa95231a1cb10b20589143a47
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-05-10 23:59:15 +0000

    added comment to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: {
+      //add the codespace domain
       allowedOrigins: ['localhost:3000','glowing-space-invention-gv9g79gx97xh9659-3000.app.github.dev']
     }
   }
