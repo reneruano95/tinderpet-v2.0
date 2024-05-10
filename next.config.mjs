691ddc0ebf9015c6f0adc44f42e36d29ba4df172
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-05-10 22:24:01 +0000

    added configuration for codespace

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000','glowing-space-invention-gv9g79gx97xh9659-3000.app.github.dev']
+    }
+  }
 };
 
 export default nextConfig;
